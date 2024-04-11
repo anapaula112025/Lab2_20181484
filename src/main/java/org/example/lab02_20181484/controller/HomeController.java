d224ad5fe7d34b9e7c2b1cd3e1aae5128de75c92
Ana <anapaula.hachi@hotmail.com>
2024-04-10 21:58:08 -0500

    second commit

--- src/main/java/org/example/lab02_20181484/controller/HomeController.java
+++ src/main/java/org/example/lab02_20181484/controller/HomeController.java
@@ -2,6 +2,7 @@ package org.example.lab02_20181484.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -12,4 +13,8 @@ public class HomeController {
     @GetMapping("/patito_hule/fotos")
     public String vistafotos(){
         return "fotos_patitos";}
+
+    @GetMapping("/patito_hule/ejecutar")
+    public String ejecutar(){
+        return "/patito_hule/fotos";}
 }
